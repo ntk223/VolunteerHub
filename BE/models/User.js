4f9def26c27285dbf0f4f7ad83efb3f785371101
ntk223 <kienlx2005@gmail.con>
2025-10-11 22:13:22 +0700

    fix

--- BE/models/User.js
+++ BE/models/User.js
@@ -10,15 +10,14 @@ const User = sequelize.define('User', {
     introduce: {type: DataTypes.TEXT, allowNull: true},
     role: {type: DataTypes.ENUM('volunteer', 'manager'), defaultValue: 'volunteer'},
     status: {type: DataTypes.ENUM('active', 'blocked'), defaultValue: 'active'},
-    created_at: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
-    updated_at: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
+    createdAt: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
+    updatedAt: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
 }, {
     tableName: 'users',
     timestamps: true,
     paranoid: true,
-    deletedAt: 'deleted_at',
-    createdAt: 'created_at',   // ✅ map lại tên cột
-    updatedAt: 'updated_at',   // ✅ map lại tên cột
+    deletedAt: 'deletedAt',
+
 
 });
 
